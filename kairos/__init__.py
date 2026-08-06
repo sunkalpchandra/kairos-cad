dbb4221bec30eb7e8df8ed19e44f34e3a69ea893
Sunkalp Chandra <sunkalp.chandra@gmail.com>
2026-08-05 20:09:03 -0400

    feat: create kairos package

---
+++ kairos/__init__.py
@@ -0,0 +1,8 @@
+"""KAIROS: Knowledge-Augmented Interactive Reinforcement Optimization System.
+
+A research prototype for sequential geometric decision-making: multimodal
+agents that learn to perform parametric CAD operations optimizing engineering
+designs under geometric, manufacturing, and performance constraints.
+"""
+
+__version__ = "0.1.0"
